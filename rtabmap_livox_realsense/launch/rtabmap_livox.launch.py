6b94369fe9a194bc06875dcbdb8d35be446624d5
XR Development <xr-dev@ll202.tail4ffe83.ts.net>
2026-01-29 14:11:05 +0100

    fix lidar only mapping and add namespaced mapping option

--- rtabmap_livox_realsense/launch/rtabmap_livox.launch.py
+++ rtabmap_livox_realsense/launch/rtabmap_livox.launch.py
@@ -11,86 +11,55 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.actions import SetParameter
 
 def generate_launch_description():
-
+    
+    # robot_namespace="leo04"
+    robot_namespace = LaunchConfiguration('robot_namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
     deskewing = LaunchConfiguration('deskewing')
-    
+
     return LaunchDescription([
 
         # Launch arguments
         DeclareLaunchArgument(
-            'use_sim_time', default_value='false',
+            'use_sim_time', default_value='true',
             description='Use simulation (Gazebo) clock if true'),
-        
+  
+        DeclareLaunchArgument(
+            'robot_namespace', 
+            default_value='leo04',
+            description='The robot namespace'),
+
         DeclareLaunchArgument(
             'deskewing', default_value='false',
             description='Enable lidar deskewing'),
-          
-        # Nodes to launch
+        
+        SetParameter(name='use_sim_time', value=LaunchConfiguration('use_sim_time')),
+        
         Node(
-            package='rtabmap_odom', executable='icp_odometry', output='screen',
+            package='rtabmap_slam', executable='rtabmap', output='screen', namespace=robot_namespace,
             parameters=[{
-              'frame_id':'base_footprint',
-              'odom_frame_id':'odom',
-              'wait_for_transform':0.2,
-              'expected_update_rate':15.0,
-              'deskewing':deskewing,
-              'use_sim_time':use_sim_time,
-              # RTAB-Map's internal parameters are strings:
-              'Icp/PointToPlane': 'true',
-              'Icp/Iterations': '10',
-              'Icp/VoxelSize': '0.1',
-              'Icp/Epsilon': '0.001',
-              'Icp/PointToPlaneK': '20',
-              'Icp/PointToPlaneRadius': '0',
-              'Icp/MaxTranslation': '2',
-              'Icp/MaxCorrespondenceDistance': '1',
-              'Icp/Strategy': '1',
-              'Icp/OutlierRatio': '0.7',
-              'Icp/CorrespondenceRatio': '0.01',
-              'Odom/ScanKeyFrameThr': '0.4',
-              'OdomF2M/ScanSubtractRadius': '0.1',
-              'OdomF2M/ScanMaxSize': '15000',
-              'OdomF2M/BundleAdjustment': 'false'
-            }],
-            remappings=[
-              ('scan_cloud', 'livox/lidar')
-            ]),
-            
-        Node(
-            package='rtabmap_util', executable='point_cloud_assembler', output='screen',
-            parameters=[{
-              'max_clouds':10,
-              'fixed_frame_id':'',
-              'use_sim_time':use_sim_time,
-            }],
-            remappings=[
-              ('cloud', 'odom_filtered_input_scan')
-            ]),
-            
-        Node(
-            package='rtabmap_slam', executable='rtabmap', output='screen',
-            parameters=[{
-              'frame_id':'base_footprint',
-              'subscribe_depth':False,
-              'subscribe_rgb':False,
+              # 'frame_id':f'{robot_namespace}/base_footprint',
+              'frame_id' : [robot_namespace, '/base_footprint'],
+              'map_frame_id' : [robot_namespace, '/map'],
               'subscribe_scan_cloud':True,
-              'approx_sync':False,
+              'subscribe_depth':False,
+              'subscribe_rgb': False,
+              'approx_sync':True,
               'wait_for_transform':0.2,
-              'use_sim_time':use_sim_time,
               # RTAB-Map's internal parameters are strings:
               'RGBD/ProximityMaxGraphDepth': '0',
               'RGBD/ProximityPathMaxNeighbors': '1',
               'RGBD/AngularUpdate': '0.05',
               'RGBD/LinearUpdate': '0.05',
-              'RGBD/CreateOccupancyGrid': 'false',
               'Mem/NotLinkedNodesKept': 'false',
               'Mem/STMSize': '30',
               'Mem/LaserScanNormalK': '20',
               'Reg/Strategy': '1',
               'Icp/VoxelSize': '0.1',
+              'Icp/RangeMin': '0.0', # Ignore laser scan points on the robot itself
               'Icp/PointToPlaneK': '20',
               'Icp/PointToPlaneRadius': '0',
               'Icp/PointToPlane': 'true',
@@ -100,28 +69,83 @@ def generate_launch_description():
               'Icp/MaxCorrespondenceDistance': '1',
               'Icp/Strategy': '1',
               'Icp/OutlierRatio': '0.7',
-              'Icp/CorrespondenceRatio': '0.2'
-            }],
+              'Icp/CorrespondenceRatio': '0.2',
+              # Occupancy Grid / Map parameters
+              'RGBD/CreateOccupancyGrid': 'true',
+              'Grid/CellSize': '0.03',
+              'Grid/Sensor': '0',
+              'Grid/MaxObstacleHeight': '1.0',
+              'Grid/MinGrounHeight':'-0.4',
+              'Grid/MaxGroundHeight':'0.07',
+              'Grid/MaxGroundAngle':'45',
+              # 'Grid/RangeMax': '2.5',
+              # 'Grid/ClusterRadious':'0.2',
+              # 'Grid/MinClusterSize':'5'
+              }],
             remappings=[
-              ('scan_cloud', 'assembled_cloud')
+              # ('scan_cloud', f'{robot_namespace}/points_color'),
+              ('scan_cloud', 'assembled_cloud'),
+
             ],
             arguments=[
               '-d' # This will delete the previous database (~/.ros/rtabmap.db)
             ]), 
-     
+
         Node(
-            package='rtabmap_viz', executable='rtabmap_viz', output='screen',
+            package='rtabmap_odom', executable='icp_odometry', output='screen', namespace=robot_namespace,
             parameters=[{
-              'frame_id':'base_footprint',
-              'odom_frame_id':'odom',
-              'subscribe_odom_info':True,
-              'subscribe_scan_cloud':True,
-              'approx_sync':False,
+              # 'frame_id':f'{robot_namespace}/base_footprint',
+              'frame_id' : [robot_namespace, '/base_footprint'],
+              'odom_frame_id' : [robot_namespace, '/odom'],
+              'wait_for_transform':0.2,
+              'expected_update_rate':15.0,
+              'deskewing':deskewing,
+              # RTAB-Map's internal parameters are strings:
+              'Icp/PointToPlane': 'true',
+              'Icp/Iterations': '10',
+              'Icp/VoxelSize': '0.1',
+              'Icp/Epsilon': '0.001',
+              'Icp/PointToPlaneK': '20',
+              'Icp/PointToPlaneRadius': '0',
+              'Icp/MaxTranslation': '2',
+              'Icp/MaxCorrespondenceDistance': '1',
+              'Icp/RangeMin': '0.0', # Ignore laser scan points on the robot itself
+              'Icp/Strategy': '1',
+              'Icp/OutlierRatio': '0.7',
+              'Icp/CorrespondenceRatio': '0.01',
+              'Odom/ScanKeyFrameThr': '0.4',
+              'OdomF2M/ScanSubtractRadius': '0.1',
+              'OdomF2M/ScanMaxSize': '15000',
+              'OdomF2M/BundleAdjustment': 'false',
+            }],
+            remappings=[
+              # ('scan_cloud', f'{robot_namespace}/livox/lidar'),
+              ('scan_cloud', 'livox/lidar'),
+            ]),
+
+        Node(
+            package='rtabmap_util', executable='point_cloud_assembler', output='screen', namespace=robot_namespace,
+            parameters=[{
+              'max_clouds':10,
+              'fixed_frame_id':'',
               'use_sim_time':use_sim_time,
             }],
             remappings=[
-               ('scan_cloud', 'odom_filtered_input_scan')
+              ('cloud', 'odom_filtered_input_scan')
             ]),
+
+
+        # Uncomment this to show the RTAB-MAP GUI 
+        # Node(
+        #     package='rtabmap_viz', executable='rtabmap_viz', output='screen',
+        #     parameters=[{
+        #       'frame_id': f'{robot_namespace}/base_footprint',
+        #       'odom_frame_id':'odom',
+        #       'subscribe_odom_info':True,
+        #       'subscribe_scan_cloud':True,
+        #       'approx_sync':False
+        #     }],
+        #     remappings=[
+        #        ('scan_cloud', 'odom_filtered_input_scan')
+        #     ]),
     ])
-    
-    
